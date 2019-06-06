0710c7ec55f4e995bd857326fc975becc88f2297
Benjamin Morrison <newben.hd@gmail.com>
2019-06-06 04:07:24 -0700

    set proxy to true to pass heroku proxy as trusted. No longer allow passport to convert https to http on callback.

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,3 @@ if (process.env.NODE_ENV === "production") {
 } else {
   module.exports = require("./dev");
 }
-
-// prod googleClientID: 268724871357-0a8a7423oovsmfov5h4kb308prjk07pv.apps.googleusercontent.com
-// prod googleClientSecret: DxLViRNza3GWn4qPO6PTUThv
-// mongoURI: mongodb+srv://admin:<password>@node-app-prod-pjrsf.mongodb.net/test?retryWrites=true&w=majority
