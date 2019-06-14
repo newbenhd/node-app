06b0a30d58f6a48169fedb25831458856742a907
Benjamin Morrison <newben.hd@gmail.com>
2019-06-14 11:15:12 -0700

    fixed to path.resolve

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    console.log(path.join(__dirname, "index.html"));
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
