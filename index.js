82854df6f699b15399d68c9b61c8bfd4457bce24
Benjamin Morrison <newben.hd@gmail.com>
2019-06-14 09:26:23 -0700

    checking if path.join(__dirname,index.html) works as I thought

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
+    console.log(path.join(__dirname, "index.html"));
     res.sendFile(path.join(__dirname, "index.html"));
   });
 }
